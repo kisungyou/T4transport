be82813ee15011c0829782154a710a241f208b73
Kisung <ksyou496@gmail.com>
2020-10-12 21:45:56 -0400

    install fixed

--- R/install_pot.R
+++ R/install_pot.R
@@ -8,7 +8,7 @@
 #' 
 #' @export
 install_pot <- function(method = "auto", conda = "auto"){
-  listed = c("numpy","cython","matplotlib","ot")
+  listed = c("numpy","cython","matplotlib","pot")
   for (i in listed){
     if (!reticulate::py_module_available(i)){
       reticulate::py_install(i, method = method, conda = conda)
